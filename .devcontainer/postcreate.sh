6f3fa0fd00c17e408eecee228eceaf59a050f971
takeharak <65995391+takeharak@users.noreply.github.com>
2023-05-15 05:22:06 +0000

    feat: add devcontainer

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# vim:sw=4:ts=4:et
+
+set -e
+
+# set git safe directory
+git config --global --add safe.directory $PWD
+
+# install global packages
+npm install -g editorconfig npm
+
+# install commitlint
+npm install -g @commitlint/cli @commitlint/config-conventional
+npx -y husky install
